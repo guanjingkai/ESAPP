ba0aea4f20885ea654e4a2108c37cc136aeeb920
雷丘 <188869727@qq.com>
2017-12-12 02:47:24 +0800

    完善支付

--- source/script/config.js
+++ source/script/config.js
@@ -7,5 +7,5 @@ module.exports.httpContentType      = "application/x-www-form-urlencoded"; //支
 module.exports.httpTimeout          = "5000";
 module.exports.httpResponseEncoding = "utf-8";
 module.exports.aliAppID = "2016092101940233";//支付宝Appid
-module.exports.aliAppID = "wx4dba82a8c817d3e7";//微信Appid
+module.exports.wxAppID = "wx4dba82a8c817d3e7";//微信Appid
 module.exports.partnerId = "1395932702";//微信商户ID
--- source/script/pay.js
+++ source/script/pay.js
@@ -4,7 +4,7 @@ var nf = d.sm("do_Notification");
 var config = require("config");
 var datacache= d.sm("do_DataCache");
 var alipay= d.sm("do_Alipay1");
-
+var weixin= d.sm("do_TencentWX");
 var serverUrl = config.serivceUrl;
 module.exports.getaway = getaway;
 
@@ -14,10 +14,10 @@ function getaway(payType,title, sn, total_amount){
 	}
 }
 function alipay_app(title,sn,total_amount){	
-	var payParam = "app_id=2014072300007148&"+
+	var payParam = "app_id="+config.aliAppID+
 		"&method=alipay.trade.app.pay"+
 		"&charset=utf-8"+
-		"&sign_type=RSA"+
+		"&sign_type=MD5"+
 		"&sign="+
 		"&timestamp="+common.formatDate(new Date())+
 		"&version=1.0"+
@@ -30,6 +30,19 @@ function alipay_app(title,sn,total_amount){
 
 	alipay.pay({
 		orderStr:payParam
+	},function(data){
+		data = JSON.parse(data);
+		nf.alert(data);
+	})
+}
+function weixin_app(title,sn,total_amount){	
+	weixin.pay({
+		appId:config.wxAppID,
+		partnerId:config.partnerId,
+		package:"",
+		noncestr:"",
+		timestamp:"",
+		sign:""
 	},function(data){
 		data = JSON.parse(data);
 		nf.alert(data);
